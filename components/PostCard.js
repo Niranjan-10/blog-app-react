ff60a8d9808ff417461a2cd3fe9d8d4e768044c2
noransoft <niranjan@noransoft.com>
2021-11-14 14:53:32 +0530

    first commit

---
+++ components/PostCard.js
@@ -0,0 +1,13 @@
+import postcss from 'postcss'
+import React from 'react'
+
+const PostCard = ({post}) => {
+    return (
+        <div>
+            {post.title}
+            {post.excerpt}
+        </div>
+    )
+}
+
+export default PostCard
